ee0b024a3e9d34e92717179291b6732232ee2d7f
ValletOr <mtvac11@gmail.com>
2021-12-11 12:15:06 +0800

    Перенос системы частиц из формы в класс-эмиттер

---
+++ Coursework/Emitter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Coursework
+{
+    public class Emitter
+    {
+        List<Particle> particles = new List<Particle>();
+        public int MousePosX = 0;
+        public int MousePosY = 0;
+
+        //Обновляем состояние частиц
+        public void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life--;
+                if (particle.Life < 0)
+                {
+                    particle.Life = 20 + Particle.rand.Next(100);
+
+                    particle.X = MousePosX;
+                    particle.Y = MousePosY;
+
+                    float direction = (float)Particle.rand.Next(360);
+                    float speed = 1 + (float)Particle.rand.Next(10);
+
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+
+                    particle.Radius = 1 + Particle.rand.Next(10);
+                }
+                else
+                {
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+                }
+            }
+            for (int i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500)
+                {
+                    var particle = new ParticleColorful();
+                    particle.Color1 = Color.DarkOrange;
+                    particle.Color0 = Color.FromArgb(0, Color.DeepSkyBlue);
+                    particle.X = MousePosX;
+                    particle.Y = MousePosY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        //Отрисовываем частицы
+        public void Render(Graphics g)
+        {
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
+    }
+}
--- Coursework/Form1.cs
+++ Coursework/Form1.cs
@@ -12,11 +12,7 @@ namespace Coursework
 {
     public partial class Form1 : Form
     {
-        List<Particle> particles = new List<Particle>();
-
-        private int MousePosX = 0;
-        private int MousePosY = 0;
-
+        public Emitter emitter1 = new Emitter();
         public Form1()
         {
             InitializeComponent();
@@ -25,70 +21,18 @@ namespace Coursework
 
         private void picDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-            MousePosX = e.X;
-            MousePosY = e.Y;
-        }
-
-        public void UpdateState()
-        {
-            foreach(var particle in particles)
-            {
-                particle.Life--;
-                if(particle.Life < 0)
-                {
-                    particle.Life = 20 + Particle.rand.Next(100);
-
-                    particle.X = MousePosX;
-                    particle.Y = MousePosY;
-
-                    float direction = (float)Particle.rand.Next(360);
-                    float speed = 1 + (float)Particle.rand.Next(10);
-
-                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
-                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
-
-                    particle.Radius = 1 + Particle.rand.Next(10);
-                }
-                else
-                {
-                    particle.X += particle.SpeedX;
-                    particle.Y += particle.SpeedY;
-                }
-            }
-            for(int i = 0; i < 10; ++i)
-            {
-                if(particles.Count < 500)
-                {
-                    var particle = new ParticleColorful();
-                    particle.Color1 = Color.DarkOrange;
-                    particle.Color0 = Color.FromArgb(0, Color.DeepSkyBlue);
-                    particle.X = MousePosX;
-                    particle.Y = MousePosY;
-                    particles.Add(particle);
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-
-        public void Render(Graphics g)
-        {
-            foreach(var particle in particles)
-            {
-                particle.Draw(g);
-            }
+            emitter1.MousePosX = e.X;
+            emitter1.MousePosY = e.Y;
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            UpdateState();
+            emitter1.UpdateState();
 
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
                 g.Clear(Color.Black);
-                Render(g);
+                emitter1.Render(g);
             }
 
             picDisplay.Invalidate();
